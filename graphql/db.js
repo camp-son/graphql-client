ac16a711d159c8e5c354c56d72d72fafdb93e31e
camp-son <sjy2814@gmail.com>
2021-12-31 14:09:16 +0900

    feat: type mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,36 +1,36 @@
-export const people = [
-  {
-    id: 1,
-    name: 'camp-son',
-    age: 31,
-    gender: 'male',
-  },
-  {
-    id: 2,
-    name: 'jys',
-    age: 30,
-    gender: 'male',
-  },
-  {
-    id: 3,
-    name: 'yjl',
-    age: 29,
-    gender: 'male',
-  },
-  {
-    id: 4,
-    name: 'bkk',
-    age: 28,
-    gender: 'male',
-  },
-  {
-    id: 5,
-    name: 'jwp',
-    age: 27,
-    gender: 'male',
-  }
+let movies = [
+  {id: 1, name: 'Spider man: No Way Home', score: 100},
+  {id: 2, name: 'Dune', score: 0},
+  {id: 3, name: 'Harry Potter', score: 74},
+  {id: 4, name: 'Logan', score: 99},
+  {id: 5, name: 'Avengers', score: 87},
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  return people.filter((person) => person.id === id)[0];
+  return movies.filter((person) => person.id === id)[0];
 };
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
+
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: movies.length + 1,
+    name,
+    score,
+  };
+
+  movies.push(newMovie);
+
+  return newMovie;
+}
